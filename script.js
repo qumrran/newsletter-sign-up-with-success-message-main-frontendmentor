f9a5188f8b2ad9dbe1647b9fa54c7ca6f212c6ba
Piotr Obara <pobara@o2.pl>
2023-08-26 16:26:04 +0200

    finish html scss, js variables created

---
+++ script.js
@@ -0,0 +1,9 @@
+const block = document.querySelector(".block");
+const popup = document.querySelector(".popup");
+
+const input = document.querySelector(".block__form__input");
+const emailAlert = document.querySelector(".block__form__label__box__error");
+
+const blockbtn = document.querySelector(".block__form__submit");
+const popupbtn = document.querySelector(".popup__button");
+
